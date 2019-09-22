56cdefd995f5c623954c68c027733314f8f43622
likunkun <9865029@qq.com>
2019-09-22 22:53:16 +0800

    修改

--- lsdapp/views.py
+++ lsdapp/views.py
@@ -38,9 +38,6 @@ def login(request):
             b = new_password.encode(encoding='utf-8')
             m.update(b)
             new_password_md5 = m.hexdigest()
-
-            # print(name, password)
-
             # 如果密码正确进行身份判断，错误则返回错误信息
             if new_password_md5 == user.password:
                 if user.authority == "boss":
@@ -61,16 +58,18 @@ def login(request):
 
 # 查看业务员列表
 def all_salesman(request):
-    response = {}
+    responses = []
     if request.method == "GET":
         # 获取所有业务员对象
         users = models.User.objects.filter(authority="salesman")
         # 循环对象
         for user in users:
+            response = {}
             # 获取姓名和电话
             response['user_name'] = user.user_name
             response['telephone'] = user.telephone
-        return JsonResponse(data=response, safe=False)
+            responses.append(response)
+        return JsonResponse(data=responses, safe=False)
 
 
 # 查看单个业务员项目个数
@@ -119,6 +118,7 @@ def add_salesman(request):
         body = json.loads(request.body)
         user_name = body['user_name']
         password = body['password']
+        telephone = body['telephone']
         # 随机生成一个字符串
         s = ''.join(random.sample(string.ascii_letters + string.digits, 10))
         # 生成一个md5对象
@@ -133,7 +133,7 @@ def add_salesman(request):
         try:
             models.UserStr.objects.create(user_name=user_name, str=s)
             # 保存经过随机字符串和MD5加密后的密码
-            models.User.objects.create(user_name=user_name, password=new_password_md5)
+            models.User.objects.create(user_name=user_name, password=new_password_md5, telephone=telephone)
         except Exception:
             response['status'] = False
             return JsonResponse(data=response, safe=False)
@@ -178,8 +178,6 @@ def add_salesman(request):
 #         return JsonResponse(data=responses, safe=False)
 
 
-# 项目搜索
-
 # 删除业务员
 def del_salesman(request):
     response = {}
@@ -187,6 +185,8 @@ def del_salesman(request):
         body = json.loads(request.body)
         user_name = body['user_name']
         user = models.User.objects.filter(user_name=user_name)
+        user_str = models.UserStr.objects.filter(user_name=user_name)
+        user_str.delete()
         user.delete()
         response['status'] = True
         return JsonResponse(data=response, safe=False)
@@ -199,13 +199,16 @@ def search_with_salesman(request):
         # 获取业务员姓名
         name = request.GET.get("user_name")
         projects_id = models.UserProject.objects.filter(user_name=name)
+        print(name)
+    if projects_id:
         for project_id in projects_id:
             single_project = {}
-            project = models.Project.objects.filter(project_id=project_id, effective=0).first()
-            single_project['project_id'] = project_id
+            project = models.Project.objects.filter(project_id=project_id.project_id, effective=0)[0]
+            print(project.project_id)
+            single_project['project_id'] = project.project_id
             single_project['project_name'] = project.project_name
             single_project['time'] = project.time
-            users = models.UserProject.objects.filter(pro_id=project_id)
+            users = models.UserProject.objects.filter(project_id=project.project_id)
             peoples = []
             # 循环放入负责人姓名
             for user in users:
@@ -213,6 +216,10 @@ def search_with_salesman(request):
             single_project['peoples'] = peoples
             responses.append(single_project)
         return JsonResponse(data=responses, safe=False)
+    else:
+        response = {}
+        response['status'] = False
+        return JsonResponse(data=response, safe=False)
 
 
 # 添加项目
@@ -220,15 +227,16 @@ def add_project(request):
     response = {}
     if request.method == "POST":
         body = json.loads(request.body)
-        project_name = body['project_name']
-        source = body['source']
+        project_name = body["project_name"]
+        source = body["source"]
         introduction = body["introduction"]
         contacts = body["contacts"]
-        telephone = body['telephone']
-        project = models.Project.objects.create(project_name=project_name, source=source, introduction=introduction,
+        telephone = body["telephone"]
+        models.Project.objects.create(project_name=project_name, source=source, introduction=introduction,
                                                 contacts=contacts, telephone=telephone)
+        project_id = models.Project.objects.filter(project_name=project_name)[0].project_id
         name = request.session['user_name']
-        models.UserProject.objects.create(project_id=project.project_id, user_name=name)
+        models.UserProject.objects.create(project_id=project_id, user_name=name)
         response['status'] = True
         return JsonResponse(data=response, safe=False)
 
@@ -275,17 +283,16 @@ def single_project_report(request):
             response["reporter"] = report.reporter
             response["time"] = report.time
             responses.append(response)
-
         # 返回汇报列表
-        return JsonResponse(data=responses, safe=False)
+        return JsonResponse(data=responses[::-1], safe=False)
 
 
 # 查看单个项目的老板留言
 def single_project_record(request):
     responses = []
     if request.method == "GET":
-        report_id = request.GET.get("report_id")
-        messages = models.Message.objects.filter(report_id=report_id)
+        project_id = request.GET.get("project_id")
+        messages = models.Message.objects.filter(project_id=project_id)
         for message in messages:
             response = {}
             response["content"] = message.content
@@ -336,17 +343,18 @@ def add_report(request):
     if request.method == 'POST':
         body = json.loads(request.body)
         # 前端获取
-        report_id = body['report_id']
+        # report_id = body['report_id']
         project_id = body['project_id']
         progress = body['progress']
         workable = body['workable']
         supply = body['supply']
         capital = body['capital']
         invoice = body['invoice']
+        report_name = body['report_name']
         other = body['other']
 
         # 插入数据库
-        models.Report.objects.create(report_id=report_id, project_id=project_id, progress=progress,
+        models.Report.objects.create(project_id=project_id, progress=progress, report_name=report_name,
                                      workable=workable, supply=supply, capital=capital, invoice=invoice, other=other)
         response['status'] = True
         return JsonResponse(data=response, safe=False)
@@ -354,7 +362,6 @@ def add_report(request):
 
 # 查看老板给业务员的消息
 def get_ones_record(request):
-    responses = []
     if request.method == "GET":
         user_name = request.GET.get("name")
 
@@ -376,7 +383,7 @@ def get_ones_record(request):
                 all_message.append(dic)
 
         # 返回一个列表，列表里每个元素是消息字典
-        return JsonResponse(data=all_message, safe=False)
+        return JsonResponse(data=all_message[::-1], safe=False)
 
 
 # 老板根据key搜索项目
@@ -408,3 +415,4 @@ def search_project(request):
                 index2 += 1
 
         return JsonResponse(data=result, safe=False)
+
